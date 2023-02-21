bfdfe4818cf67271a415261487e28e8f63c444d0
Luke Gary <luke@modern-age.com>
2023-02-21 17:46:08 -0500

    caching via proxy

--- proxy/app/controllers/schedule_controller.rb
+++ proxy/app/controllers/schedule_controller.rb
@@ -1,8 +1,26 @@
-# The controller to hand the /Schedule requests.
 class ScheduleController < ApplicationController
-  before_action :valid_patient_check
-
   def index
-    get_fhir("/Schedule")
+    provider_ids = params[:provider_ids]
+    location_id = params[:location_id]
+    date = params[:date]
+    duration = params[:duration]
+    
+    cache_key = "schedule/#{provider_ids}/#{location_id}/#{date}/#{duration}"
+
+    if Rails.cache.exist?(cache_key)
+      response = Rails.cache.read(cache_key)
+    else
+      response = fetch_schedule(provider_ids, location_id, date, duration)
+
+      Rails.cache.write(cache_key, response, expires_in: 1.hour)
+    end
+
+    render json: response
+  end
+
+  private
+
+  def fetch_schedule(provider_ids, location_id, date, duration)
+    # Fetch the data from the database
   end
 end
